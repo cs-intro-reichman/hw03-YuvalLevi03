62953b5d1717512da4b0e9be23d520d45e8dfa1b
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2024-01-06 12:52:21 +0200

    Update UniqueChars

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,12 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+           String ans = "";
+           for(int i = 0 ; i < s.length() ; i++ ) {
+               if (s.indexOf(s.charAt(i)) == i || s.charAt(i) == ' ') { 
+                   ans +=  s.charAt(i); 
+               }
+            }
+            return ans;
     }
 }
